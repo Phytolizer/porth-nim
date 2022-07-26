c503200f90c3482ffc7ed5e215c76aa044797a38
Kyle Coffey <me@phytolizer.dev>
2022-07-25 19:22:14 -0500

    error reporting

--- src/porthpkg/parse.nim
+++ src/porthpkg/parse.nim
@@ -1,15 +1,20 @@
 from operation import Operation, plus, minus, dump, push
 from std/strutils import parseInt
 from token import Token
+import std/strformat
 
 proc parseTokenAsOp*(token: Token): Operation =
     case token.text
     of "+":
-        plus()
+        return plus()
     of "-":
-        minus()
+        return minus()
     of ".":
-        dump()
+        return dump()
     else:
-        let value = token.text.parseInt()
-        push(value)
+        try:
+            let value = token.text.parseInt()
+            return push(value)
+        except ValueError:
+            stderr.writeLine fmt"{token.filePath}:{token.line}:{token.column}: {getCurrentExceptionMsg()}"
+            quit(1)
